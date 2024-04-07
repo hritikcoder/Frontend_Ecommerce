2a2d9279804f3367b4636f3c23c8ae98ca7bd8a9
hritikcoder <92904159+hritikcoder@users.noreply.github.com>
2024-04-07 11:15:15 +0530

    all file add

---
+++ src/pages/Admin/ProductList.jsx
@@ -0,0 +1,169 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useCreateProductMutation,
+} from "../../redux/api/productApiSlice";
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
+import { toast } from "react-toastify";
+import AdminMenu from "./AdminMenu";
+
+const ProductList = () => {
+  const [imgUrl, setImgUrl] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [brand, setBrand] = useState("");
+  const [stock, setStock] = useState(0);
+  const [imageUrl, setImageUrl] = useState(null);
+  const navigate = useNavigate();
+
+  const [createProduct] = useCreateProductMutation();
+  const { data: categories } = useFetchCategoriesQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const productData = new FormData();
+      // modified 
+      productData.append("imgUrl", imgUrl);
+
+
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("category", category);
+      productData.append("quantity", quantity);
+      productData.append("brand", brand);
+      productData.append("countInStock", stock);
+
+      const { data } = await createProduct(productData);
+
+      if (data.error) {
+        toast.error("Product create failed. Try Again.");
+      } else {
+        toast.success(`${data.name} is created`);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Product create failed. Try Again.");
+    }
+  };
+ 
+
+  return (
+    <div className="container xl:mx-[8rem] sm:mx-[0] ">
+      <div className="flex flex-col md:flex-row ">
+        <AdminMenu />
+        <div className="md:w-3/4 p-2">
+          <div className="h-10">Create Product</div>
+
+          
+
+          <div className="one">
+                <label htmlFor="name">Image Url</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={imgUrl}
+                  onChange={(e) => setImgUrl(e.target.value)}
+                />
+            </div>
+
+          <div className="p-3">
+            <div className="flex flex-wrap">
+            
+              <div className="one">
+                <label htmlFor="name">Name</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Price</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="one">
+                <label htmlFor="name block">Quantity</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Brand</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <label htmlFor="" className="my-5">
+              Description
+            </label>
+            <textarea
+              type="text"
+              className="p-2 mb-3 bg-[#101011] border rounded-lg w-[98%] text-white"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+
+            <div className="flex justify-between">
+              <div>
+                <label htmlFor="name block">Count In Stock</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  value={stock}
+                  onChange={(e) => setStock(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="">Category</label> <br />
+                <select
+                  placeholder="Choose Category"
+                  className="p-4 mb-3 w-[26rem] border rounded-lg bg-[#101011] text-white"
+                  onChange={(e) => setCategory(e.target.value)}
+                >
+                  {categories?.map((c) => (
+                    <option key={c._id} value={c._id}>
+                      {c.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="py-4 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    proxy: {
+
+      // "/api/":"https://e-commerce-mern-zzpm.onrender.com/"
+
+
+     "/api/": "http://localhost:5000",
+   
+    }
+  }
+})
